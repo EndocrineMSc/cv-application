714769147f530c3467050e3ee2c7ff2f866d098e
endocrine <117483689+EndocrineMSc@users.noreply.github.com>
2024-02-23 16:28:05 +0100

    feat: Add basic outline for an input form for timespans

--- src/components/Timespan.jsx
+++ src/components/Timespan.jsx
@@ -1,20 +1,82 @@
-import {v4 as uuidv4} from "uuid";
+import { v4 as uuidv4 } from 'uuid';
 
-function Timespan(props) {
-    return (
-        <>
-            <h2 className="timespan">
-                {props.startYear} - {props.endYear ? props.endYear : "jetzt"} &middot; {props.institute} &middot; {props.title}
-            </h2>
-            <ul className="timespan">
-                {props.details ? 
-                props.details.map(detail => (
-                    <li key={uuidv4()}>{detail}</li>
-                ))
-            : <></>}
-            </ul>
-        </>
-    )
+function Timespan({
+  startYear, endYear, institute, title, details,
+}) {
+  return (
+    <>
+      <h2 className="timespan">
+        {startYear}
+        {' '}
+        -
+        {' '}
+        {endYear || 'jetzt'}
+        {' '}
+        &middot;
+        {' '}
+        {institute}
+        {' '}
+        &middot;
+        {' '}
+        {title}
+      </h2>
+      <ul className="timespan">
+        {details
+          ? details.map((detail) => (
+            <li key={uuidv4()}>{detail}</li>
+          ))
+          : null}
+      </ul>
+      <TimespanForm {...{
+        startYear, endYear, institute, title,
+      }}
+      />
+    </>
+  );
+}
+
+function TimespanForm({
+  startYear, endYear, institute, title,
+}) {
+  return (
+    <div className="formContainer">
+      <label htmlFor="formStartYear">
+        Arbeitsbeginn (Jahr)
+        <input
+          type="text"
+          id="formStartYear"
+          defaultValue={startYear || ''}
+        />
+      </label>
+
+      <label htmlFor="formEndYear">
+        Arbeitsende (Jahr/Leer)
+        <input
+          type="text"
+          id="formEndYear"
+          defaultValue={endYear || ''}
+        />
+      </label>
+
+      <label htmlFor="formInstitute">
+        Institut
+        <input
+          type="text"
+          id="formInstitute"
+          defaultValue={institute || ''}
+        />
+      </label>
+
+      <label htmlFor="formTitle">
+        Bezeichnung
+        <input
+          type="text"
+          id="formTitle"
+          defaultValue={title || ''}
+        />
+      </label>
+    </div>
+  );
 }
 
 export default Timespan;
